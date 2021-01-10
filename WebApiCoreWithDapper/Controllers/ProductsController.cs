6443eda29cddfde88e0add6ccf6c0527dd028386
Aydın Türker <doktoralban@outlook.com>
2021-01-10 11:13:45 +0300

    Add project files.

---
+++ WebApiCoreWithDapper/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApiCoreWithDapper.Data;
+
+namespace WebApiCoreWithDapper.Controllers
+{
+    [Route("[controller]")]
+    [ApiController] 
+    
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly dbContextNW _context;
+        private readonly SqlConnection connection;
+        public ProductsController(dbContextNW context)
+        {
+            _context = context;
+
+            connection = new SqlConnection(_context.Database.GetConnectionString());
+        }
+
+
+        //************************************* 
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IEnumerable<Products>> GetAllProductsAsync()
+        {
+            string sql = "SELECT * FROM dbo.PRODUCTS";
+            return await connection.QueryAsync<Products>(sql); 
+            //return connection.Query<Products>(sql).ToList(); 
+        }
+
+        //*************************************
+        [HttpGet]
+      
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+         
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProduct(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ WebApiCoreWithDapper/Data/dbContextNW.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCoreWithDapper.Data
+{
+	public partial class dbContextNW : DbContext
+	{
+		public dbContextNW(DbContextOptions<dbContextNW> options) : base(options)
+		{
+
+		}
+
+
+		public DbSet<Products> Products { get; set; }
+
+
+	}
+}
